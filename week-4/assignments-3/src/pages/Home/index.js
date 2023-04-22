a2e5843d88809016d65bb3a6985e0335806d051a
Surfer-Alex <alex22277716@gmail.com>
2023-04-23 01:54:34 +0800

    add assignments 4

--- week-4/assignments-3/src/pages/Home/index.js
+++ week-4/assignments-3/src/pages/Home/index.js
@@ -1,13 +1,14 @@
-import Body from "./components/Body"
-import Header from "./components/Header"
-import "./index.css" 
-
+import Body from "./components/Body";
+import Header from "./components/Header";
+import "./index.css";
 
 const Home = () => {
-    return <div>
-        <Header />
-        <Body />
+  return (
+    <div>
+      <Header />
+      <Body />
     </div>
-}
+  );
+};
 
 export default Home;
---
+++ week-4/assignments-4/src/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { useEffect, useState } from "react";
+import "./style.css";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.github.com/orgs/facebook/repos?per_page=5&page=${page}`
+        );
+        const parsedData = await res.json();
+        setLoading(false);
+        setData((pre) => [...pre, ...parsedData]);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getData();
+  }, [page]);
+
+  return (
+    <div className="div">
+      <div className="header">
+        {data &&
+          data.map((items) => (
+            <div className="eader">
+              <p className="button">
+                <h2>{items.name}</h2>
+                <h5 className="visi">{items.visibility}</h5>
+              </p>
+              <br />
+              <p className="para">{items.description}</p>
+              <br />
+              <p>
+                {items.topics.map((topic) => (
+                  <p className="topics">{topic}</p>
+                ))}
+              </p>
+            </div>
+          ))}
+      </div>
+      <button className="button move" onClick={() => setPage(page + 1)}>
+        <h2>{loading ? "...載入中" : "More"}</h2>
+      </button>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("here"));
